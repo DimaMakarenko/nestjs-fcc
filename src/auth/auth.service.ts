b10ab48b1f4287c0d83ecbd2b410550c16edc9fe
DimaMakarenko <makarenko.d.viri@gmail.com>
2023-11-04 20:56:34 +0200

    save user to db by sign up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDTO } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -9,7 +11,26 @@ export class AuthService {
   signIn() {
     return 'Sign In Service';
   }
-  signUp(dto: AuthDTO) {
-    return 'Sign Up Service';
+  async signUp(dto: AuthDTO) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exist');
+        }
+      }
+      throw new Error(error);
+    }
   }
 }
